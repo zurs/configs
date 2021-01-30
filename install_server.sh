77806e08b481a33d40c90519285a7cd9ba2b8e33
Hampus <hampusek96@gmail.com>
2021-01-30 16:11:10 +0100

    Fix broken install script again 4

--- install_server.sh
+++ install_server.sh
@@ -2,7 +2,8 @@ sudo apt install curl git neovim tmux -y
 
 cd $HOME
 curl https://raw.githubusercontent.com/zurs/configs/master/.tmux.conf > .tmux.conf
+tmux kill-server
 
-mkdir config
-mkdir config/nvim
+mkdir .config
+mkdir .config/nvim
 curl https://raw.githubusercontent.com/zurs/configs/master/init.vim > config/nvim/init.vim
