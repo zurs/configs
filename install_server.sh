ef1150c4afdb99aa0f984db47460cde65db43af6
Hampus <hampusek96@gmail.com>
2021-01-30 15:45:22 +0100

    Fix broken install script again

--- install_server.sh
+++ install_server.sh
@@ -1,10 +1,10 @@
 sudo apt install git neovim tmux -y
 
 cd $HOME
-wget https://raw.githubusercontent.com/zurs/configs/master/.tmux.conf
+wget https://raw.githubusercontent.com/zurs/configs/master/.tmux.conf -O .tmux.conf
 
 mkdir config
 cd config
 mkdir nvim
 cd nvim
-wget https://raw.githubusercontent.com/zurs/configs/master/init.vim
+wget https://raw.githubusercontent.com/zurs/configs/master/init.vim -O init.vim
